0134f56857508b791e306f5a3fd3aa8d92dc2916
MichaelPaulukonis <xraysmalevich@gmail.com>
2025-11-10 08:44:49 -0500

    Updates

---
+++ assets/version-constants-CEhLuIcw.js
@@ -0,0 +1,1 @@
+const o="0.4.0",t="2025-11-10T13:44:44.668Z";export{o as APP_VERSION,t as BUILD_TIME};
--- assets/version-constants-XHC84Oir.js
+++
@@ -1,1 +0,0 @@
-const o="0.3.0",t="2025-11-06T13:55:30.387Z";export{o as APP_VERSION,t as BUILD_TIME};
