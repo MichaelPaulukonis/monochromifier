c9051b9e3b91bd523627f2fcbb9a120675232b27
Michael Paul Ukonis <michael@example.com>
2026-03-20 23:53:34 -0400

    Updates

--- assets/version-constants-BXLuWgIh.js
+++
@@ -1,1 +0,0 @@
-const o="0.7.0",t="2026-03-20T04:00:35.174Z";export{o as APP_VERSION,t as BUILD_TIME};
---
+++ assets/version-constants-DXuj4CK-.js
@@ -0,0 +1,1 @@
+const o="0.8.0",t="2026-03-21T03:53:30.387Z";export{o as APP_VERSION,t as BUILD_TIME};
